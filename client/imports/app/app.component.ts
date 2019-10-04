69d67bd2c99e6850b9928b6ddb9e64b0da15cafe
Evgheni Curdov <kurdov.e@gmail.com>
2019-10-03 19:22:06 -0700

    still debagging

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Rx';
 import { Parties } from '../../../both/collections/parties.collection';
 import { Party } from '../../../both/models/party.model';
  
